69d867282c93f8d82206ede1200574026197eecc
Bujenita Lucian-Andrei <49783911+luciiscoding@users.noreply.github.com>
2025-04-10 15:07:14 +0300

    Add files via upload

---
+++ Application/UseCases/CommandHandlers/CreatePropertyCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Application.UseCases.Commands;
+using Domain.Entities;
+using Fluent.Infrastructure.FluentModel;
+using System.Data.Entity.Infrastructure;
+
+namespace Application.UseCases.CommandHandlers
+{
+    public class CreatePropertyCommandHandler : IRequestHandler<CreatePropertyCommand, int>
+    {
+        public  ApplicationDbContext _context;
+
+        public CreatePropertyCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreatePropertyCommand request, CancellationToken cancellationToken)
+        {
+            if (_context == null)
+            {
+                throw new InvalidOperationException("Database context is not initialized.");
+            }
+
+            var property = new Property
+            {
+                Address = request.Address,
+                Description = request.Description,
+                Price = request.Price,
+                Type = request.Type,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+            _context.Set<Property>().Add(property);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return property.Id.GetHashCode();
+        }
+
+
+    }
+    }
---
+++ Application/UseCases/Queries/GetAllPropertiesQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Application.DTOs;
+using Application.UseCases.QueryHandlers;
+using MediatR;
+
+namespace Application.UseCases.Queries
+{
+    public class GetAllPropertiesQuery : IRequest<List<PropertyDto>>
+    {
+    }
+}
